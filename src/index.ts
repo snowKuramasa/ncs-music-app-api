4351c34db5f82833974a8d2d192f5e08c60af758
kuramasa <kuramasa.green@gmail.com>
2024-11-24 21:13:11 +0900

    cors 設定

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express from 'express'
+import cors from 'cors' // CORSパッケージのインポート
 import { PrismaClient } from '@prisma/client'
 
 const app = express()
 const prisma = new PrismaClient()
 
+app.use(cors()) // CORSを有効にする
+app.use(
+  cors({
+    origin: ['https://ncs-music-app-api.vercel.app'],
+  })
+)
+
 app.use(express.json())
 
-app.get('/hello', async (req, res) => {
-  res.json({ Hello: 'World' })
-})
-
-// Create
+// CRUDエンドポイントの定義
 app.post('/users', async (req, res) => {
   const { name, email } = req.body
   const user = await prisma.user.create({
@@ -19,7 +23,6 @@ app.post('/users', async (req, res) => {
   res.json(user)
 })
 
-// Read
 app.get('/users', async (req, res) => {
   const users = await prisma.user.findMany()
   res.json(users)
@@ -33,7 +36,6 @@ app.get('/users/:id', async (req, res) => {
   res.json(user)
 })
 
-// Update
 app.put('/users/:id', async (req, res) => {
   const { id } = req.params
   const { name, email } = req.body
@@ -44,7 +46,6 @@ app.put('/users/:id', async (req, res) => {
   res.json(user)
 })
 
-// Delete
 app.delete('/users/:id', async (req, res) => {
   const { id } = req.params
   const user = await prisma.user.delete({
